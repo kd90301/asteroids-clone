fd27d1a5f29b65b250283a6c3f99ea17634bd9db
kd90301 <kyle.p.darling@gmail.com>
2025-03-01 19:17:14 -0800

    Add enemy damage mechanic
    When enemy collision area touches player, player is queue_free

--- asteroids-clone/scenes/player.gd
+++ asteroids-clone/scenes/player.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 @export var max_speed := 100.0
 var velocity := Vector2(0,0)
+var steering_factor := 10.0
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
@@ -11,8 +15,15 @@ func _process(delta: float) -> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
-		rotation = velocity.angle()
+		get_node("Sprite2D").rotation = velocity.angle()
+
+func _on_area_entered(area_that_entered : Area2D) -> void:
+	if area_that_entered.is_in_group("enemy"):
+		queue_free()
