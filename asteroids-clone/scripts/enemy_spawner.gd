cb00bbc3f51e9386c5cacc55fa59179a19b65add
kd90301 <kyle.p.darling@gmail.com>
2025-03-02 16:06:04 -0800

    Add enemy spawner
    Spawns enemy scenes at random inside viewport - final version will spawn enemies outside screen and move them toward the player

---
+++ asteroids-clone/scripts/enemy_spawner.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var enemy = preload("res://scenes/enemy.tscn")
+
+func _ready() -> void:
+	get_node("Timer").timeout.connect(on_timer_timeout)
+	
+func on_timer_timeout() -> void:
+	var enemy_instance := enemy.instantiate()
+	add_child(enemy_instance)
+	var viewport_size := get_viewport_rect().size
+	var random_position := Vector2(0, 0)
+	random_position.x = randf_range(0, viewport_size.x)
+	random_position.y = randf_range(0, viewport_size.y)
+	enemy_instance.position = random_position
