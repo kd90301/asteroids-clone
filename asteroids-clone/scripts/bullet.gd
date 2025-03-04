187dcf100267940dddf5936fa3ba1fe1f0c97bb1
kd90301 <kyle.p.darling@gmail.com>
2025-03-04 15:56:39 -0800

    Update bullet scene
    will now queue free when colliding with enemies

--- asteroids-clone/scripts/bullet.gd
+++ asteroids-clone/scripts/bullet.gd
@@ -6,7 +6,9 @@ extends Area2D
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
 
+func _ready() -> void:
+	area_entered.connect(on_bullet_object_entered)
+
 func on_bullet_object_entered(object_entered) -> void:
-	if object_entered.is_in_group("enemy"):
-		object_entered.queue_free()
 	queue_free()
+		
--- asteroids-clone/scripts/enemy.gd
+++ asteroids-clone/scripts/enemy.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
+@export var velocity : Vector2 = Vector2(50,50)
+
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
+
 func _on_area_entered(area_that_entered : Area2D) -> void:
 	if area_that_entered.is_in_group("bullet"):
 		queue_free()
+
+func _process(delta: float) -> void:
+
+	pass
+	
